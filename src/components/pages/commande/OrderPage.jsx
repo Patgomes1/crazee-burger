10157f2fb3d19718899d0f41c6fa047b37cc4878
Patgomes1 <71064665+Patgomes1@users.noreply.github.com>
2025-12-11 14:44:24 -0800

    📦 F02: Add React Router

---
+++ src/components/pages/commande/OrderPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function OrderPage() {
+  return (
+    <div>
+        <h1>"`Bonjour ${}`"</h1>
+        <button>Déconnexion</button>
+    </div>
+  )
+}
