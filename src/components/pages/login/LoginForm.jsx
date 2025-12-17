54a18657f2af54b8ee4a4c3caac19fd65b2da914
Patgomes1 <71064665+Patgomes1@users.noreply.github.com>
2025-12-17 13:49:41 -0800

    💄F03 : Add design system file and CSS

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import styled from 'styled-components'
 
 
 export default function LoginForm({onSuccess}) {
@@ -20,12 +21,24 @@ export default function LoginForm({onSuccess}) {
 
     //Affichage
   return (
-    <form 
+    <LoginFormStyled 
     onSubmit={handleSubmit}>
           <h1>Bienvenue chez nous !</h1>
           <h2>Connectez-vous</h2>
-          <input value={username} type="text" name="username" placeholder='Entrez votre prenom' onChange={handleChange} required />
-          <button type="submit">Accéder à votre espace</button>
-    </form>
+          <input value={username} type="text" name="username" placeholder='Entrez votre prénom' onChange={handleChange} required />
+          <button type="submit">Accéder à mon espace</button>
+    </LoginFormStyled>
   )
 }
+
+//styles
+const LoginFormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2rem;
+  border: 1px solid #eee;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  color: red;
+`;
