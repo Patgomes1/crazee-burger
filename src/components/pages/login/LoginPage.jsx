4c74341dc1bfe92e11d90f0db3eb8954750861bf
Patgomes1 <71064665+Patgomes1@users.noreply.github.com>
2025-12-22 12:43:41 -0800

    💄F03 : LoginPage > Update style

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -3,7 +3,7 @@ import LoginForm from './LoginForm'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { theme } from '../../../theme'
-import { LoginPageStyled } from './LoginPageStyled'
+//import { LoginPageStyled } from './LoginPageStyled'
 // import Logo from './Logo'
 import Logo from "../../reusable-ui/Logo";
 
@@ -29,22 +29,19 @@ export default function LoginPage() {
     )
 }
 
-/* const LoginPageStyled = styled.div`min-height: 100vh;
-  width: 100%;
+const LoginPageStyled = styled.div`
+  min-height: 100dvh;
+  width: 100vw;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  background-image:
-    linear-gradient(
-      rgba(0, 0, 0, 0.5),
-      rgba(0, 0, 0, 0.5)
-    ),
-    url(${burgerBg});
-
+  background-image: url("src/assets/burger-bg.jpg");
   background-size: cover;
   background-position: center;
-`; */
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-blend-mode: darken;
+`;
 
 
