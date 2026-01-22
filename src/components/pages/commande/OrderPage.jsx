523ec706eeb4a79bab5751b0ac8f68214996266e
Patgomes1 <71064665+Patgomes1@users.noreply.github.com>
2026-01-22 07:50:15 -0800

    🐛 F04 : ORDER > Corrected page display with width from % to vw

--- src/components/pages/commande/OrderPage.jsx
+++ src/components/pages/commande/OrderPage.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import { useState } from 'react'
-import { useNavigate, useLocation } from 'react-router-dom'
-
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import { theme } from '../../../theme';
 
 export default function OrderPage() {
   //State
-    const navigate = useNavigate()
-    const location = useLocation()   // ← hook à ajouter
-    const username = location.state?.username || "Erreur utilisateur"
+  const navigate = useNavigate();
+  const location = useLocation(); // ← hook à ajouter
+  const username = location.state?.username || 'Erreur utilisateur';
 
-    //Comportement
-    const handleClick = () => {
-        navigate("/")
-    }
-    //console.log("username = " + username);
+  //Comportement
+  const handleClick = () => {
+    navigate('/');
+  };
+  //console.log("username = " + username);
 
-    //Affichage
+  //Affichage
   return (
-    <div>
-        <h1>Bonjour {username}</h1>
-        <button onClick={handleClick}>Déconnexion</button>
-    </div>
-  )
+    <OrderPageStyled>
+      <h1>Bonjour {username}</h1>
+      <button onClick={handleClick}>Déconnexion</button>
+    </OrderPageStyled>
+  );
 }
+
+const OrderPageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  width: 100vw;
+`;
