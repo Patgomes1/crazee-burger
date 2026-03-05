2e5fd1f4fd7c9fa098a36105fb43a0799f9f057d
Patgomes1 <71064665+Patgomes1@users.noreply.github.com>
2026-03-05 15:02:35 +0100

    💄 F07 : Add layout with basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -6,24 +6,45 @@ import Basket from './Basket';
 export default function Main() {
   return (
     <MainStyled>
-      <Basket />
-      <Menu />
+      {/* <Basket /> */}
+      <div className="menu-and-admin">
+        <Menu />
+        <div className="admin">admin</div>
+      </div>
     </MainStyled>
   );
 }
 
 const MainStyled = styled.div`
   background: ${theme.colors.background_white};
-  flex: 1;
+  height: calc(95vh - 10vh);
+
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   display: grid;
-  grid-template-columns: 25% 1fr;
+  /* grid-template-columns: 25% 1fr; */
+  grid-template-columns: 1fr;
   overflow-y: scroll;
 
   .basket {
     background: pink;
+  }
+
+  .menu-and-admin {
+    position: relative;
+    overflow-y: hidden;
+    display: grid;
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+    .admin {
+      height: 250px;
+      background: ${theme.colors.background_dark};
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
   }
 `;
