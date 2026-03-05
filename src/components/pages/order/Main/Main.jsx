a8826a59d35b5cc3d7510c9c83a5b148e0a9480e
Patgomes1 <71064665+Patgomes1@users.noreply.github.com>
2026-03-05 14:23:27 +0100

    ♻️ F07 : Update PrimaryButton color and add <basket/> to main

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -6,21 +6,24 @@ import Basket from './Basket';
 export default function Main() {
   return (
     <MainStyled>
-      {/* <Basket /> */}
+      <Basket />
       <Menu />
     </MainStyled>
   );
 }
 
 const MainStyled = styled.div`
-  flex-grow: 1;
-  background: ${theme.colors.lightGray};
-  color: ${theme.colors.black};
+  background: ${theme.colors.background_white};
+  flex: 1;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
-  background: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns: 25% 1fr;
+  overflow-y: scroll;
+
+  .basket {
+    background: pink;
+  }
 `;
