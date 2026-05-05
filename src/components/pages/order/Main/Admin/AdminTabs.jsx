bbd673c87316181dc0c6940ca656ce9f6ae26088
Patgomes1 <71064665+Patgomes1@users.noreply.github.com>
2026-05-05 14:16:58 +0200

    💄 F07 : ADMIN > Add Tabs to AdminTabs

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import Tab from '../../../../reusable-ui/Tab';
+import { IoChevronForwardOutline } from 'react-icons/io5';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
-export default function AdminTabs() {
-  return <AdminTabsStyled>AdminTabs</AdminTabsStyled>;
+export default function AdminTabs({}) {
+  return (
+    <AdminTabsStyled>
+      <Tab
+        Icon={<FiChevronDown className="icon" />}
+        //label={'Ajouter un produit'}
+      />
+      <Tab
+        className={'ajouter'}
+        Icon={<IoChevronForwardOutline className="icon" />}
+        label={'Ajouter un produit'}
+      />
+      <Tab className={'modifier'} label={'Modifier un produit'} />
+    </AdminTabsStyled>
+  );
 }
 
 const AdminTabsStyled = styled.div`
-  border: 1px solid red;
-  height: 43px;
-  width: 132px;
+  //border: 1px solid red;
+  color: black;
+  display: flex;
+  flex-direction: row;
+  .icon {
+    width: 16px;
+    height: 16px;
+  }
+  .ajouter {
+    background-color: lightgreen;
+  }
+  .modifier {
+    background-color: lightblue;
+  }
 `;
---
+++ src/components/reusable-ui/Tab.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { theme } from '../../theme';
+
+export default function Tab({ className, Icon, label }) {
+  return (
+    <TabStyled className={className}>
+      {Icon && Icon}
+      <span>{label}</span>
+    </TabStyled>
+  );
+}
+
+const TabStyled = styled.button`
+  height: 43px;
+  padding: 0 22px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: ${theme.fonts.size.P0};
+  color: ${theme.colors.greySemiDark};
+  background: ${theme.colors.white};
+  border-width: 1px 1px 2px 1px;
+  border-style: solid;
+  border-color: ${theme.colors.greyLight};
+  box-shadow: ${theme.shadows.subtle};
+  border-radius: ${theme.borderRadius.round} ${theme.borderRadius.round} 0 0;
+
+  position: relative;
+  left: 5%;
+  top: 1px;
+
+  &:hover {
+    border-bottom: 2px solid ${theme.colors.white};
+  }
+
+  &:active {
+    background: ${theme.colors.primary};
+    color: ${theme.colors.white};
+  }
+
+  .icon {
+    display: flex;
+  }
+`;
